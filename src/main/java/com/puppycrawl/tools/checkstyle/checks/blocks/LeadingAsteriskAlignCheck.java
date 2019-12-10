68f49ca8feb20520f34cfdb0be981bec9db69589
alexman258 <alexman258@gmail.com>
2019-12-10 13:47:06 -0800

    Defined necessary tokens. Added more stubs

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/LeadingAsteriskAlignCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/LeadingAsteriskAlignCheck.java
@@ -20,6 +20,9 @@
 package com.puppycrawl.tools.checkstyle.checks.blocks;
 
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.JavadocTokenTypes;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 //import com.puppycrawl.tools.checkstyle.api.AbstractCheck.FileContext;
 import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
 import java.io.BufferedReader;
@@ -29,26 +32,95 @@ import java.io.IOException;
 /**
  * <p>
  * Leading Asterisk {@code LeadingAsteriskAlignCheck} checks that every 
- * Javadoc comment leading asterisk is vertically aligned. 
+ * multi-line comment leading asterisk is vertically aligned.  
+ * 
+ * Javadoc comments have a LeadingAsterisk token that can be compared with the
+ * leading asterisk token of every other line in the comment.
+ * 
+ * Regular block comments do not contain a leading asterisk token. 
+ * The leading asterisk is found programmatically and compared with
+ * other leading asterisks in the comment block.
+ * 
  * </p>
  */
 public class LeadingAsteriskAlignCheck extends AbstractCheck {
 
+	/**
+	* Ok, left
+	* 
+	*/
+	
+	/**
+	 * Ok, center
+	 * 
+	 */
+	
+	/**
+	  * Ok, left
+	  * 
+	  */
+	
 
 	@Override
+	public boolean isCommentNodesRequired() {
+		return true;
+	}
+	
+	@Override
 	public int[] getDefaultTokens() {
-		return null;
+		return getAcceptableTokens();
 	}
 
 	@Override
 	public int[] getAcceptableTokens() {
-		return null;
+		return new int[] {TokenTypes.BLOCK_COMMENT_BEGIN, TokenTypes.BLOCK_COMMENT_END};
 	}
 
 	@Override
 	public final int[] getRequiredTokens() {
-		return null;
+		return new int[] {TokenTypes.BLOCK_COMMENT_BEGIN, TokenTypes.BLOCK_COMMENT_END};
+	}
+	
+	@Override
+	public void visitToken(DetailAST ast) {
+		
+		if(ast.getType() == TokenTypes.BLOCK_COMMENT_BEGIN) {
+			
+			DetailAST firstChild = ast.getFirstChild();
+			
+			if (firstChild == null) {
+				return;
+			}
+			
+			DetailAST secondChild = firstChild.getFirstChild();
+			
+			if (secondChild.getType() == JavadocTokenTypes.JAVADOC) {
+				checkJavadocCommentBlock(ast, ast.getLastChild());
+			} else {
+				checkRegularCommentBlock(ast, ast.getLastChild());
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 * 
+	 * @param blockBegin The beginning token of the comment block.
+	 * @param blockEnd The end token of the comment block
+	 */		
+	public void checkRegularCommentBlock(DetailAST blockBegin, DetailAST blockEnd) {
+		
+	}
+	
+	
+	/**
+	 * 
+	 * @param blockBegin The beginning token of the comment block.
+	 * @param blockEnd The end token of the comment block
+	 */		  
+	public void checkJavadocCommentBlock(DetailAST blockBegin, DetailAST blockEnd) {
+		
 	}
 
-	
 }
