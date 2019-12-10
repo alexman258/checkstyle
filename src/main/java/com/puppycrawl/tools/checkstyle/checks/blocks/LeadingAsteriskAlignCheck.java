0fc7657102181c7749bc631e6071598868d15914
alexman258 <alexman258@gmail.com>
2019-12-10 12:24:02 -0800

    Moved class to more appropriate location

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/LeadingAsteriskAlignCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/LeadingAsteriskAlignCheck.java
@@ -17,7 +17,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
 
-package com.puppycrawl.tools.checkstyle.checks.javadoc;
+package com.puppycrawl.tools.checkstyle.checks.blocks;
 
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 //import com.puppycrawl.tools.checkstyle.api.AbstractCheck.FileContext;
